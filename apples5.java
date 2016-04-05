b267e7d84051f35955dc2e803e855f27d9d62ee5
Joseph Nash <jnash@companieshouse.gov.uk>
2016-04-05 16:23:39 +0100

    this is the first commit

---
+++ apples5.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.util.Scanner;
+
+class apples5{
+	public static void main(String args[]){
+		Scanner joseph = new Scanner(System.in);
+		
+		int girls, boys, people;
+		girls = 7;
+		boys = 3;
+		people= girls % boys;
+		System.out.println(people);
+	}
+}
---
+++ pears.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.Scanner;
+
+class pears {
+	public static void main(String args[]){
+		Scanner joseph = new Scanner(System.in);
+		
+		int tuna = 5;
+		int bass = 18;
+		
+		tuna += 8;
+		System.out.println(tuna);
+		/* easier than the way below. takes original value and adds 8
+		 * -= *= /=*/
+		
+		/*tuna = tuna + 5;
+		System.out.println(tuna);*,
+		
+		/*System.out.println(tuna++);
+		int total = tuna + bass;
+		System.out.println(total);*/
+	}
+}
